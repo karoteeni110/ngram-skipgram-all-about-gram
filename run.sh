d2b92c990dab47578af58934de833d30c938e2bc
karoteeni110 <30997769+karoteeni110@users.noreply.github.com>
2019-03-19 20:40:17 +0200

    run.sh for taito GPU

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+#SBATCH -J hello_SLURM
+#SBATCH -o output.txt
+#SBATCH -e errors.txt
+#SBATCH -t 01:20:00
+#SBATCH -n 1
+#SBATCH -p gpu
+#SBATCH --mem=4096
+
+module purge
+module load python-env/intelpython3.6-2018.3 gcc/5.4.0 cuda/9.0 cudnn/7.1-cuda9
+
+python3 original.py
