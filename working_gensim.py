7f6384cddd3437b3d2532e7a6ad89c56629bdc56
karoteeni110 <30997769+karoteeni110@users.noreply.github.com>
2019-04-14 18:51:54 +0300

    A workable gensim

---
+++ working_gensim.py
@@ -0,0 +1,17 @@
+from gensim.models import Word2Vec
+from random import randint, shuffle
+
+EPOCHS=10
+
+data = open("newtxt.txt").read().split('\n')
+data = [line.strip(' ').split(' ') for line in data]
+
+for i in range(5):
+    shuffle(data)
+    model = Word2Vec(data, size=100, window=5, min_count=1, workers=4, seed=randint(1,100))
+    model.train(data, total_examples=len(data), epochs=EPOCHS)
+
+    print("Most similar wordforms to %s in iteration %u:" % ("lēg",i))
+    for wf, score in model.most_similar("lēg"):
+        print(wf,score)
+    print()
